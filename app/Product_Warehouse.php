369dbd74aab19ccac89c3457e795c325b75d0ee7
dhaydt <1992dayat@gmail.com>
2023-07-17 21:23:34 +0700

    undefined warehouse

--- app/Product_Warehouse.php
+++ app/Product_Warehouse.php
@@ -9,7 +9,6 @@ class Product_Warehouse extends Model
 {
 	protected $table = 'product_warehouse';
     protected $fillable =[
-
         "product_id", "product_batch_id", "variant_id", "imei_number", "warehouse_id", "qty", "price"
     ];
 
